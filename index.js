ac243327800217e526ce0cd0a00a1d34e64e2e06
ProgrammerSM <silvertone_1987@yahoo.com>
2022-08-12 15:52:20 -0600

    Update index.js

--- index.js
+++ index.js
@@ -11,28 +11,11 @@ module.exports = {
       },
       create: (context) => ({
         Program: (node) => {
-          if (
-            node.source.value === 'react' &&
-            // Only look for imports of react itself, not `import type`
-            node.importKind === 'value'
-          ) {
-            // If there's only one default import, we can make an autofix
-            if (node.specifiers.length === 1 && node.specifiers[0].type === 'ImportDefaultSpecifier') {
-              context.report({
-                node,
-                messageId: "match",
-                fix(fixer) {
-                  return fixer.replaceText(node.specifiers[0], '* as React');
-                },
-              });
-            }
-            // Mixing default and named imports, cannot autofix
-            else if (node.specifiers.some((specifier) => specifier.type === 'ImportDefaultSpecifier')) {
-              context.report({
-                node,
-                messageId: "match",
-              });
-            }
+          if (node.local.name === 'React') {
+            context.report(
+              node,
+              messageId: 'match'
+            );
           }
         },
       }),
