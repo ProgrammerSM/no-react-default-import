180dedee4cb8ee93f77bb2476a8138def7a5f479
ProgrammerSM <silvertone_1987@yahoo.com>
2022-08-12 14:56:08 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,31 +1,36 @@
 module.exports = {
-  rules: {
-    "no-testing": {
-      meta: {
-        type: "problem",
-        docs: {
-          description:
-            "If you knew what you were doing, you wouldn't need to test it",
-        },
-        messages: {
-          match:
-            "Found '{{filename}}' .spec file. If you knew what you were doing, you wouldn't need to test it.",
-        },
-      },
-      create: (context) => ({
-        Program: (node) => {
-          const filename = context.getFilename();
-          if (filename.includes(".spec")) {
+  meta: {
+    type: 'suggestion',
+    fixable: 'code',
+    schema: [],
+  },
+  create(context) {
+    return {
+      ImportDeclaration(node) {
+        if (
+          node.source.value === 'react' &&
+          // Only look for imports of react itself, not `import type`
+          node.importKind === 'value'
+        ) {
+          // If there's only one default import, we can make an autofix
+          if (node.specifiers.length === 1 && node.specifiers[0].type === 'ImportDefaultSpecifier') {
             context.report({
               node,
-              messageId: "match",
-              data: {
-                filename,
+              message: 'Do not use the default React import, use named or namespace import instead.',
+              fix(fixer) {
+                return fixer.replaceText(node.specifiers[0], '* as React');
               },
             });
           }
-        },
-      }),
-    },
+          // Mixing default and named imports, cannot autofix
+          else if (node.specifiers.some((specifier) => specifier.type === 'ImportDefaultSpecifier')) {
+            context.report({
+              node,
+              message: 'Do not use the default React import, use named or namespace import instead.',
+            });
+          }
+        }
+      },
+    };
   },
 };
