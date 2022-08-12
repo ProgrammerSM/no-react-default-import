c34f947dda409dc1467f4a9a92aa6a07766fe695
ProgrammerSM <silvertone_1987@yahoo.com>
2022-08-12 16:06:41 -0600

    Update index.js

--- index.js
+++ index.js
@@ -9,14 +9,33 @@ module.exports.rules = {
       schema: [],
     },
     create(context) {
-      ImportDefaultSpecifier(node) {
-        if (node.local.name === 'React') {
-          context.report(
-            node,
-            'Do not import React, use import deconstruction instead',
-          );
-        }
-      },
+      return {
+        ImportDeclaration(node) {
+          if (
+            node.source.value === 'react' &&
+            // Only look for imports of react itself, not `import type`
+            node.importKind === 'value'
+          ) {
+            // If there's only one default import, we can make an autofix
+            if (node.specifiers.length === 1 && node.specifiers[0].type === 'ImportDefaultSpecifier') {
+              context.report({
+                node,
+                message: 'Do not use the default React import, use named or namespace import instead.',
+                fix(fixer) {
+                  return fixer.replaceText(node.specifiers[0], '* as React');
+                },
+              });
+            }
+            // Mixing default and named imports, cannot autofix
+            else if (node.specifiers.some((specifier) => specifier.type === 'ImportDefaultSpecifier')) {
+              context.report({
+                node,
+                message: 'Do not use the default React import, use named or namespace import instead.',
+              });
+            }
+          }
+        },
+      };
     },
   }
 };
