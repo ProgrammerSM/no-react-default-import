523b69ad44f1e36e350d346a551d1b150ffdd68a
ProgrammerSM <silvertone_1987@yahoo.com>
2022-08-12 16:04:08 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,24 +1,22 @@
-module.exports = {
-  rules: {
-    "no-react-default": {
-      meta: {
-        type: 'problem',
-        fixable: 'code',
-        messages: {
-          match: 'Do not use the default React import, use named or namespace import instead.'
-        },
-        schema: [],
+module.exports.rules = {
+  "no-react-default": {
+    meta: {
+      type: 'problem',
+      fixable: 'code',
+      messages: {
+        match: 'Do not use the default React import, use named or namespace import instead.'
       },
-      create: (context) => ({
-        Program: (node) => {
-          if (node.local.name === 'React') {
-            context.report(
-              node,
-              messageId: 'match'
-            );
-          }
-        },
-      }),
-    }
+      schema: [],
+    },
+    create(context) {
+      ImportDefaultSpecifier(node) {
+        if (node.local.name === 'React') {
+          context.report(
+            node,
+            'Do not import React, use import deconstruction instead',
+          );
+        }
+      },
+    },
   }
 };
